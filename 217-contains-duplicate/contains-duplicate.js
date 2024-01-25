29fa2abe770d3d417f44aef52e231b2447577ce6
Paras Jain <31515109+paras5669@users.noreply.github.com>
2024-01-25 09:15:19 +0530

    Time: 81 ms (59.17%) | Memory: 61.5 MB (7.56%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+
+   let numMap = {};
+
+   for(let i = 0 ; i < nums.length ; i++){
+
+       if(numMap[nums[i]] !== undefined){
+           return true;
+       }
+
+       numMap[nums[i]] = i;
+
+   }
+
+   return false;
+
+};
